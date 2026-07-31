c465e8ee3d136ebe2472ae0f3f29d3a8ef84a9e8
Anna Ordog <aordog@uwo.ca>
2026-07-31 17:28:43 -0400

    add tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,31 @@
 """Tests for statistics functions within the Model layer."""
 
 import pandas as pd
+import pytest
 
+@pytest.mark.parametrize(
+    "test_df, test_colname, expected",
+    [
+        (pd.DataFrame(data=[[1, 5, 3], 
+                            [7, 8, 9], 
+                            [3, 4, 1]], 
+                      columns=list("abc")),
+        "a",
+        7),
+        (pd.DataFrame(data=[[0, 0, 0], 
+                            [0, 0, 0], 
+                            [0, 0, 0]], 
+                      columns=list("abc")),
+        "b",
+        0),
+    ])
+
+def test_max_mag(test_df, test_colname, expected):
+    """Test max function works for array of zeroes and positive integers."""
+    from lcanalyzer.models import max_mag
+    assert max_mag(test_df, test_colname) == expected
+
+'''
 def test_max_mag_integers():
     # Test that max_mag function works for integers
     from lcanalyzer.models import max_mag
@@ -12,7 +36,7 @@ def test_max_mag_integers():
     test_input_colname = "a"
     test_output = 7
 
-    assert max_mag(test_input_df, test_input_colname) == test_output
+    assert max_mag(test_input_df, test_input_colname) == test_output, 'Max mag is incorrect'
 
 def test_max_mag_zeros():
     # Test that max_mag function works for zeros
@@ -25,3 +49,16 @@ def test_max_mag_zeros():
     test_output = 0
 
     assert max_mag(test_input_df, test_input_colname) == test_output
+
+def test_min_mag_ints():
+
+    from lcanalyzer.models import min_mag
+
+    test_input_df = pd.DataFrame(data=[[1, 5, 3], 
+                                       [7, 8, 9], 
+                                       [3, 4, 1]], columns=list("abc"))
+    test_input_colname = "a"
+    test_output = 1
+
+    assert min_mag(test_input_df, test_input_colname) == test_output
+'''
